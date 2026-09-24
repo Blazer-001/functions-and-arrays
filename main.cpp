70e7b26c20e372bb45f0cea2dbcaba701672f64a
Blazer-001 <elzhan.pashaev1090@gmail.com>
2026-09-24 20:48:08 +0300

    СВÑыполнил 1-е задание

--- main.cpp
+++ main.cpp
@@ -1,5 +1,30 @@
 #include <iostream>
 
+// task 1
+void ElementAboveAvg() {
+    int n;
+    std::cin >> n;
+
+    int* arr = new int[n];
+    long long sum = 0;
+
+    for (int i{}; i < n; ++i) {
+        std::cin >> arr[i];
+        sum += arr[i];
+    }
+    double average = (double) sum/n;
+    std::cout << "Average: " << average << '\n';
+    std::cout << "Above average: ";
+
+    for (int i{}; i < n; ++i) {
+        if (arr[i] > average) {
+            std::cout << arr[i] << " ";
+        }
+    }
+    delete[] arr;
+}
+
+
 // task 2
 void FirstPosition() {
     int n{}, x{};
@@ -26,8 +51,29 @@ void FirstPosition() {
     delete[] arr;
 }
 
+
+// task 3
+void ReplaceNegatives(int* arr,int size, std::string message = "") {
+    if (!message.empty())
+        std::cout << message << '\n';
+
+    for (int i{}; i < size; ++i) {
+        std::cout << arr[i] << ' ';
+    }
+    std::cout << '\n';
+}
+
+
 int main() {
+    // task 1
+    ElementAboveAvg();
+
+
     // task 2
     FirstPosition();
     return 0;
+
+    // task 3
+    ReplaceNegatives();
+
 }
