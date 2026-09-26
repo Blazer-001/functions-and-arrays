0f0db417946e26d1556adaf5ab8fa8a98a49b606
Slasher <elzhan.pashaev1090@gmail.com>
2026-09-26 14:33:44 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -53,11 +53,7 @@ void FirstPosition() {
 
 
 // task 5
-<<<<<<< HEAD
 void ReplaceNegatives(int *arr, int size, std::string message = "") {
-=======
-void ReplaceNegatives(int* arr,int size, std::string message = "") {
->>>>>>> d1ae83ffa8b79d6a6843b1c633ee9067879e8040
     if (!message.empty())
         std::cout << message << '\n';
 
@@ -87,11 +83,7 @@ void ArrayReversal() {
 int main() {
     // task 1
     ElementAboveAvg();
-<<<<<<< HEAD
 
-=======
-    
->>>>>>> d1ae83ffa8b79d6a6843b1c633ee9067879e8040
 
     // task 2
     FirstPosition();
